6cbd333525082d160db16e51d3daca5e98679a33
Janaina Mota <janaother16@gmail.com>
2024-09-23 20:23:38 -0300

    Initial commit

---
+++ api-chatbot/src/main/java/com/partiutech/api_chatbot/ApiChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.partiutech.api_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChatbotApplication.class, args);
+	}
+
+}
---
+++ api-chatbot/src/test/java/com/partiutech/api_chatbot/ApiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partiutech.api_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
